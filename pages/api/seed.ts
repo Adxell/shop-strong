57df1a972cb31d40e5e1a861d56bffb9cc7a15ab
adxell-arango-clickonplans <90437102+Adxell@users.noreply.github.com>
2022-10-11 12:40:59 -0500

    make endpoint for produtcs

---
+++ pages/api/seed.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, seedDB } from '../../database'
+import { Product } from '../../models'
+
+
+type Data = {
+  message: string
+}
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    if ( process.env.NODE_ENV === 'production' ) {
+        return res.status(401).json({message: 'Dont have access to service'})
+    }
+    db.connect()
+    await Product.deleteMany()
+    await Product.insertMany(seedDB.initialData.products)
+    db.disconnect()
+    res.status(200).json({ message: 'Seed exetuted' })
+}
+
