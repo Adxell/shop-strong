670a6b0d571e65ef2bd6825d4d3234d261633864
adxell <adxell02@gmail.com>
2022-10-11 20:10:26 -0500

    added FullScreenLoading a provider informacion

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,23 @@ import type { NextPage } from 'next'
 
 import { ShopLayout } from '../components/layout'
 import { Typography } from '@mui/material'
-import { initialData } from '../database/products'
 import { ProductList } from '../components/products'
+import { useProducts } from '../hooks'
+import { FullScreenLoading } from '../components/ui'
 
 
 const Home: NextPage = () => {
+  const { products, isLoading, isError } = useProducts('products')
+
   return (
     <ShopLayout title={'SHOP-COM'} pageDescription={'Tus soluciones'}>
       <Typography variant='h1' component='h1'>Tienda</Typography> 
       <Typography variant='h2' sx={{ mb: 1}}>Todos los productos</Typography> 
-
-      <ProductList
-        products={ initialData.products as any}
-      />
+      {
+        isLoading
+          ? <FullScreenLoading />
+          : <ProductList products={ products } />
+      }
     </ShopLayout>
   )
 }
