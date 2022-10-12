cd06649a31971a0bf3ddcaebba052a6ebcdf5acb
adxell <adxell02@gmail.com>
2022-10-12 16:11:12 -0500

    make page by slug

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,12 +1,20 @@
-import { Box, Button, Chip, Grid, Typography } from '@mui/material'
-import React from 'react'
+import React, { FC } from 'react'
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+
+import { Box, Button, Grid, Typography } from '@mui/material'
+
 import { ShopLayout } from '../../components/layout'
 import { ProductSlideShow, SizeSelector } from '../../components/products'
 import { ItemCounter } from '../../components/ui'
-import { initialData } from '../../database/products'
-const product = initialData.products[1]
-
-const ProductPage = () => {
+import { IProduct } from '../../interfaces'
+import { dbProducts } from '../../database'
+interface Props {
+  product: IProduct
+}
+const ProductPage: FC<Props> = ({ product }) => {
+  // const router = useRouter();
+  // const {products: product, isLoading, isError} = useProducts<IProduct>(`/products/${ router.query.slug }`)
+  
   return (
     <ShopLayout title={ product.title } pageDescription={ product.description }>
        <Grid container spacing={ 3 }>
@@ -39,4 +47,56 @@ const ProductPage = () => {
   )
 }
 
+// no usar server side rendering 
+// export const getServerSideProps:GetServerSideProps = async({ params }) =>{
+
+//   const {slug = '' } = params as { slug: string }
+//   const product = await dbProducts.getProductBySlug( slug )
+  
+//   if ( !product ) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false
+//       }
+//     }
+//   }
+  
+//   return {
+//     props:{
+//       product
+//     }
+//   }
+
+// }
+
+export const getStaticPaths: GetStaticPaths = async (ctx) =>{
+  const slugs = await dbProducts.getAllProductSlugs()
+  return{
+    paths: slugs.map(({ slug })=>({
+      params: {slug}
+    })),
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps:GetStaticProps = async({params}) =>{
+
+  const {slug} = params as {slug: string}
+  const product = await dbProducts.getProductBySlug(slug)
+  if ( !product ) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props:{
+      product
+    },
+    revalidate: 60*60*24
+  }
+}
 export default ProductPage
