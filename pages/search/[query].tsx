50881c6fcc4f59a60a2c64c2554caf3a297f7753
adxell <adxell02@gmail.com>
2022-10-12 19:10:18 -0500

    set config SSR

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,23 +1,32 @@
 import type { GetServerSideProps, NextPage } from 'next'
 
 import { ShopLayout } from '../../components/layout'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { ProductList } from '../../components/products'
-import { useProducts } from '../../hooks'
-import { FullScreenLoading } from '../../components/ui'
 import { dbProducts } from '../../database'
 import { IProduct } from '../../interfaces'
 
 interface Props {
     products: IProduct[];
+    foundProducts: boolean;
+    query: string;
 }
 
-const QueryPage: NextPage<Props> = ({ products }) => {
+const QueryPage: NextPage<Props> = ({ products, foundProducts, query }) => {
 
   return (
     <ShopLayout title={'SHOP-Search'} pageDescription={'resultados de busqueda'}>
       <Typography variant='h1' component='h1'>Buscar</Typography> 
-      <Typography variant='h2' sx={{ mb: 1}}>Todos los productos</Typography> 
+      {
+        foundProducts 
+            ? <Typography variant='h2' sx={{ mb: 1}}>Busqueda: { query }</Typography> 
+            : (
+                <Box display='flex' >
+                    <Typography variant='h2' sx={{ mb: 1}} textTransform='capitalize'>No se encontro ningun producto</Typography> 
+                    <Typography variant='h2' sx={{ ml: 1}} color='secondary' textTransform='capitalize'>{ query }</Typography> 
+                </Box>
+            )
+      }
         <ProductList products={ products } />
     </ShopLayout>
   )
@@ -36,12 +45,16 @@ export const getServerSideProps:GetServerSideProps = async({ params }) =>{
   }
 
   let products = await dbProducts.getProductByTerm( query )
-  
+  const foundProducts = products.length > 0; 
   // TODO: retornar otros productos
-  
+  if ( !foundProducts ) {
+    products = await dbProducts.getAllProducts()
+  }
   return {
     props:{
-        products
+        products,
+        foundProducts,
+        query
     }
   }
 
