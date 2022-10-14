e3714d4ba89b2e5c0281bce6d5ae3c31df550c63
adxell <adxell02@gmail.com>
2022-10-14 00:47:30 -0500

    added provider auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@emotion/react'
 import { lightTheme } from '../themes'
 import { CssBaseline } from '@mui/material'
 import { SWRConfig } from 'swr'
-import { CartProvider, UiProvider } from '../context'
+import { AuthProvider, CartProvider, UiProvider } from '../context'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -13,14 +13,16 @@ function MyApp({ Component, pageProps }: AppProps) {
       value={{
         fetcher: (resource, init) => fetch(resource, init).then( res => res.json())
       }}
-    > <CartProvider>
-        <UiProvider>
-          <ThemeProvider theme={lightTheme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </UiProvider>
-      </CartProvider>
+    > <AuthProvider>
+        <CartProvider>
+          <UiProvider>
+            <ThemeProvider theme={lightTheme}>
+              <CssBaseline />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </UiProvider>
+        </CartProvider>
+      </AuthProvider>
     </SWRConfig>
   )
 }
