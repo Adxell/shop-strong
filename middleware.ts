3ae674d356268d7732c7f16439464d6aa954d058
adxell <adxell02@gmail.com>
2022-10-15 02:13:44 -0500

    create middleware checkout pages

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as jose from 'jose'
+
+
+export async function middleware(req:NextRequest) {
+    try {
+        await jose.jwtVerify( req.cookies.get('token') as string, 
+             new TextEncoder().encode(process.env.JWT_SECRET_SEED)
+            )
+
+            return NextResponse.next()
+    } catch( error ) {
+        return NextResponse.redirect(`http://localhost:3000/auth/login?p=${req.nextUrl.pathname}`)
+    }
+}
+
+export const config = {
+    matcher: '/checkout/:path*',
+}
