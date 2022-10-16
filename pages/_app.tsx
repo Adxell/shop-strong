1bd239c965341d022b49105219ab9b31c678434b
adxell <adxell02@gmail.com>
2022-10-16 01:29:31 -0500

    set session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import {SessionProvider} from 'next-auth/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react'
@@ -6,24 +7,25 @@ import { CssBaseline } from '@mui/material'
 import { SWRConfig } from 'swr'
 import { AuthProvider, CartProvider, UiProvider } from '../context'
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig 
-      value={{
-        fetcher: (resource, init) => fetch(resource, init).then( res => res.json())
-      }}
-    > <AuthProvider>
-        <CartProvider>
-          <UiProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </UiProvider>
-        </CartProvider>
-      </AuthProvider>
-    </SWRConfig>
+    <SessionProvider session={(pageProps as any).session}>
+      <SWRConfig 
+        value={{
+          fetcher: (resource, init) => fetch(resource, init).then( res => res.json())
+        }}
+      > <AuthProvider>
+          <CartProvider>
+            <UiProvider>
+              <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </UiProvider>
+          </CartProvider>
+        </AuthProvider>
+      </SWRConfig>
+    </SessionProvider>
   )
 }
 
