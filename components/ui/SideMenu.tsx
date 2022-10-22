de08330c68dc7a18dd10e6d5560d76be37786ca3
adxell <adxell02@gmail.com>
2022-10-22 01:50:25 -0500

    set navigateto user

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -150,14 +150,20 @@ export const SideMenu = () => {
                                 </ListItemIcon>
                                 <ListItemText primary={'Productos'} />
                             </ListItem>
-                            <ListItem button>
+                            <ListItem 
+                                button
+                                onClick={() => navigateTo('/admin/orders')}
+                            >
                                 <ListItemIcon>
                                     <ConfirmationNumberOutlined/>
                                 </ListItemIcon>
                                 <ListItemText primary={'Ordenes'} />
                             </ListItem>
 
-                            <ListItem button>
+                            <ListItem 
+                                button
+                                onClick={() => navigateTo('/admin/user')}
+                            >
                                 <ListItemIcon>
                                     <AdminPanelSettings/>
                                 </ListItemIcon>
